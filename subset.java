020130b5e5f999cbfaf91c179d38f867bbda388c
Sai <92389813+oohsai@users.noreply.github.com>
2023-10-27 13:20:23 +0530

    Add files via upload

---
+++ subset.java
@@ -0,0 +1,18 @@
+package recursion_subset;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class subset {
+    public static void main(String[] args) {
+
+    }
+
+    static List<List<Integer>> sub(int[] arr ) {
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+
+        return outer;
+    }
+}
